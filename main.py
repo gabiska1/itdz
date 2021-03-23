95530e91d8db7833a8f1a81b0172301c6a5f5795
lillil <k1903@list.ru>
2021-03-23 19:43:43 +0300

    Заменил.

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("HELLO")
+st = input("Введите строку: ")
+
+wLen = 0  # Длина текущего слова
+maxLen = 0  # Длина самого длинного слова
+minLen = 30000  # Длина самого короткого слова
+
+for s in st:
+    if s == ' ':
+        if maxLen < wLen:
+            maxLen = wLen
+        if (minLen > wLen) and (wLen != 0):
+            minLen = wLen
+        wLen = 0
+        continue
+    wLen += 1
+
+print("Длина самого длинного слова: " + str(maxLen))
+print("Длина самого короткого слова: " + str(minLen))
